66126588bffe44f793d0b59a149d048cc194f079
Justin Mills <MillsJROSS@gmail.com>
2017-08-02 19:47:06 -0500

    Added basic testing around account management

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -8,7 +8,7 @@ RSpec.feature "HomePage", type: :feature do
     expect(page).to have_selector("header", text: "Lister")
     expect(page).to have_selector("header .btn", text: "Login")
     expect(page).to have_selector("main #about")
-    expect(page).to have_selector("main #signup .btn", text: "Signup")
+    expect(page).to have_selector("main #signup .btn", text: "Sign up")
     expect(page).to have_selector("footer a[href='https://github.com/codingjaunt/']")
     expect(page).to have_selector("footer a[href='www.codingjaunt.com']")
   end
