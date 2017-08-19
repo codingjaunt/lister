7368942dcbe251d92ed95205af44f21b14b3c9bd
Justin Mills <MillsJROSS@gmail.com>
2017-08-19 09:20:27 -0500

    Making prettier front end

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -5,8 +5,8 @@ RSpec.feature "HomePage", type: :feature do
     visit root_path
 
     expect(page).to have_title("Home | Lister")
-    expect(page).to have_selector("header", text: "Lister")
-    expect(page).to have_selector("header .btn", text: "Login")
+    expect(page).to have_selector("nav", text: "Lister")
+    expect(page).to have_selector("nav a", text: "Login")
     expect(page).to have_selector("main #about")
     expect(page).to have_selector("main #signup .btn", text: "Sign up")
     expect(page).to have_selector("footer a[href='https://github.com/codingjaunt/']")
